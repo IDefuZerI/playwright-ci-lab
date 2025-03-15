205064f34501c41429a7549bea1572f5bba2ddb6
IDefuZerI <109233850+IDefuZerI@users.noreply.github.com>
2025-03-15 19:29:03 +0200

    V0.4

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,7 @@
 // playwright.config.js
 const config = {
-    testDir: './tests', // Папка з тестами
-    timeout: 30000, // Таймаут для тестів
-    use: {
-      headless: false, // Запуск у режимі з браузером
-      viewport: { width: 1280, height: 720 },
-    },
-  };
-  
-  module.exports = config;
+  testDir: './tests',
+  reporter: [['html', { outputFolder: 'playwright-report' }]],
+};
+
+module.exports = config;
